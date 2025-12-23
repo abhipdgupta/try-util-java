d6bdd8eaba16a5aefe234a3fc75e6f89797a0e0c
Abhishek Prasad Gupta <abhipdgupt007@gmail.com>
2025-12-23 13:01:40 +0530

    initial commit

---
+++ src/main/java/io/github/abhipdgupta/App.java
@@ -0,0 +1,13 @@
+package io.github.abhipdgupta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
